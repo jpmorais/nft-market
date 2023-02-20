cc78d149f72290f4be05cba4e70eee2888697a17
jpmorais <jpmorais@gmail.com>
2023-02-20 19:11:04 -0300

    openai included

--- controllers/openai-controller.js
+++ controllers/openai-controller.js
@@ -1,5 +1,26 @@
-const newImage = async (req, res) => {
-    res.send("Create new Image")
-}
+const { Configuration, OpenAIApi } = require("openai");
 
-module.exports = {newImage}
+const newImage = async (req, res) => {
+
+  try {
+    const configuration = new Configuration({
+      apiKey: process.env.OPENAI_KEY,
+    });
+    const openai = new OpenAIApi(configuration);
+  
+    const {prompt} = req.body
+    console.log(`prompt ${prompt}`)
+  
+    const response = await openai.createImage({
+      prompt: prompt,
+      n: 1,
+      size: "512x512",
+    });
+  
+    res.send(response.data["data"][0]["url"]);      
+  } catch (error) {
+    res.status(500).send("Error ", error)
+  }
+};
+
+module.exports = { newImage };
